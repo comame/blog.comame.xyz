7df9348507fa9325a23345619ae0d0611998292a
comame <dev@comame.xyz>
2020-03-19 19:34:04 +0900

    愉快なログ

---
+++ assets/js/log.js
@@ -0,0 +1,6 @@
+{
+    const date = document.querySelector("#build-status").title
+    console.log(`Bulid: ${date}`)
+    console.log(`GitHub Repo: https://github.com/comame/blog.comame.xyz`)
+    console.log(`ブログについての技術的まとめ: https://blog.comame.xyz/entries/2020-03-11/blog-tech.html`)
+}
