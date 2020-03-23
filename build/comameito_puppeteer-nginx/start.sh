235987f0d6eb10aee23b7007a172138a75706a82
comame <dev@comame.xyz>
2020-03-23 16:00:16 +0900

    コンテナを改良

--- build/comameito_puppeteer-nginx/start.sh
+++ build/comameito_puppeteer-nginx/start.sh
@@ -1,9 +1,16 @@
+#!/usr/bin/env sh
+
+service nginx start
+
+if [ "$1" = "serve" ]; then
+    read -p "Press any key to terminate: " tmp
+    exit 0
+fi
+
 cd /
 cp /files/package.json /package.json
 cp /files/package-lock.json /package-lock.json
 
 npm ci
-
-service nginx start
 
 ALL=1 node /files/build
