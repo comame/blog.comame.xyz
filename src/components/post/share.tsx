3f4bc6761814d0174694bb2e27755815c097f19a
comame <dev@comame.xyz>
2020-12-25 04:05:08 +0900

    copy

--- src/components/post/share.tsx
+++ src/components/post/share.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useState } from 'react'
 import { Entry } from '../../lib/entry'
 import styles from '../../styles/post/share.module.scss'
 
@@ -6,6 +6,24 @@ const Share: FunctionComponent<{ entry: Entry }> = ({ entry }) => {
     const href = 'https://blog.comame.xyz/entries/' + entry.date + '/' + entry.entry
     const twitterUrl = 'https://twitter.com/intent/tweet?text='+ encodeURIComponent(entry.title) + '%0a&url=' + encodeURIComponent(href) + '&related=comameito'
     const facebookUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(href)
+
+    const [ isCopiedShown, setIsCopiedShown ] = useState(false)
+    const copy = () => {
+        if (isCopiedShown) return
+        setIsCopiedShown(true)
+
+        const tmp = document.createElement('div')
+        tmp.setAttribute('style', 'position: fixed; left: -100%;')
+        tmp.appendChild(document.createElement("pre")).textContent = "https://" + location.host + location.pathname;
+        document.body.appendChild(tmp)
+        document.getSelection()?.selectAllChildren(tmp)
+        document.execCommand('copy')
+        document.body.removeChild(tmp)
+
+        setTimeout(() => {
+            setIsCopiedShown(false)
+        }, 3000)
+    }
 
     return <div className={ styles.share }>
         <img alt='共有' src='/icons/share.svg' />
@@ -15,7 +33,10 @@ const Share: FunctionComponent<{ entry: Entry }> = ({ entry }) => {
         <a rel='noopener' target='_blank' title='Facebook で共有' href={ facebookUrl }>
             <img alt='Facebook で共有' src='/icons/facebook_logo.svg' />
         </a>
-        <script async src='/js/url-copy.js'></script>
+        <a title='URL をコピー' onClick={ copy }>
+            <img alt='URL をコピー' src='/icons/link.svg' />
+        </a>
+        <div id={ styles.copied } className={ isCopiedShown ? styles.shown : '' }>URL をコピーしました</div>
     </div>
 }
 
