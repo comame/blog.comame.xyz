1f98b2777b255a86150c27fd66a84668ff9de2ac
comame <dev@comame.xyz>
2020-12-24 18:50:49 +0900

    ビルドログの文面

--- src/lib/build/build.js
+++ src/lib/build/build.js
@@ -8,4 +8,6 @@ void (() => {
     const dir = path.resolve(__dirname + '../../../../docs')
     fs.writeFileSync(dir + '/' + 'feed.xml', feed, 'utf8')
     fs.writeFileSync(dir + '/' + 'sitemap.txt', sitemap, 'utf8')
+
+    console.log('RSS and sitemap generated.')
 })()
